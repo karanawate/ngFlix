897079f9cdd63f09f0ae13ae4454e6d2f25ed2fd
karanawate <karanawate8@gmail.com>
2024-03-17 20:24:17 +0530

    httpClinet using get data and console.data using subscribe method

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.scss'
+  styleUrl: './slider.component.scss',
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    console.log('Hello');
+    this.http
+      .get(
+        'https://api.themoviedb.org/3/movie/popular?api_key=415483c86436e6dfc6155f24a0d752fa'
+      )
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
 }
