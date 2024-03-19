a931af1826e014f80a7f69dfe39af4a6883a956f
karanawate <karanawate8@gmail.com>
2024-03-19 19:36:59 +0530

    MODEL TYPE USING

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MovieDto } from '../types/movie';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,7 @@ export class MoviesService {
   constructor(private http: HttpClient) {}
 
   getPopularMovies() {
-    return this.http.get<any>(
+    return this.http.get<MovieDto>(
       'https://api.themoviedb.org/3/movie/popular?api_key=415483c86436e6dfc6155f24a0d752fa'
     );
   }
