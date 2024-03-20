a24037106a57b725a3158fa7894499e8e2dd629f
karanawate <karanawate8@gmail.com>
2024-03-20 17:54:22 +0530

    BroserAnimatios Library for using animations

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MoviesService } from '../../services/movies.service';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
+  animations: [
+    trigger('slideFade', [
+      state('void', style({ opacity: 0 })),
+      transition('void <=> *', [animate('1s')]),
+    ]),
+  ],
 })
 export class SliderComponent implements OnInit {
   constructor(private moviesService: MoviesService) {}
