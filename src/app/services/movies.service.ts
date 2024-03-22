f68d2c30aaad5cf8f94f9bfdc1e46bf3c3ca2e44
karanawate <karanawate8@gmail.com>
2024-03-22 10:03:36 +0530

    RXJS library using map function side call function called

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MovieDto } from '../types/movie';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,23 +12,29 @@ export class MoviesService {
   constructor(private http: HttpClient) {}
 
   getPopularMovies() {
-    return this.http.get<MovieDto>(
-      `${this.apiUrl}/movie/popular?api_key=${this.apikey}`
-      // 'https://api.themoviedb.org/3/movie/popular?api_key=415483c86436e6dfc6155f24a0d752fa'
-    );
+    return this.http
+      .get<MovieDto>(
+        `${this.apiUrl}/movie/popular?api_key=${this.apikey}`
+        // 'https://api.themoviedb.org/3/movie/popular?api_key=415483c86436e6dfc6155f24a0d752fa'
+      )
+      .pipe(map((data) => data.results));
   }
 
   getUpcomingMovies() {
-    return this.http.get<MovieDto>(
-      `${this.apiUrl}/movie/upcoming?api_key=${this.apikey}`
-      // https://api.themoviedb.org/3/movie/upcoming?api_key=415483c86436e6dfc6155f24a0d752fa
-    );
+    return this.http
+      .get<MovieDto>(
+        `${this.apiUrl}/movie/upcoming?api_key=${this.apikey}`
+        // https://api.themoviedb.org/3/movie/upcoming?api_key=415483c86436e6dfc6155f24a0d752fa
+      )
+      .pipe(map((data) => data.results));
   }
 
   getTopRatedMovies() {
-    return this.http.get<MovieDto>(
-      `${this.apiUrl}/movie/top_rated?api_key=${this.apikey}`
-      //https://api.themoviedb.org/3/tv/top_rated?api_key=415483c86436e6dfc6155f24a0d752fa
-    );
+    return this.http
+      .get<MovieDto>(
+        `${this.apiUrl}/movie/top_rated?api_key=${this.apikey}`
+        //https://api.themoviedb.org/3/tv/top_rated?api_key=415483c86436e6dfc6155f24a0d752fa
+      )
+      .pipe(map((data) => data.results));
   }
 }
