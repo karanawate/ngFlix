6a5829ec56e1c00c0f8a0a1799c90133d2ad8686
karanawate <karanawate8@gmail.com>
2024-03-23 09:11:03 +0530

    click on show-item on card

---
+++ src/app/pages/show-details/show-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-details',
+  templateUrl: './show-details.component.html',
+  styleUrl: './show-details.component.scss'
+})
+export class ShowDetailsComponent {
+
+}
