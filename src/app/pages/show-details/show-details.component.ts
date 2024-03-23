547f8d32d9e946d33cfb567c12d7eaa3b541c292
karanawate <karanawate8@gmail.com>
2024-03-23 12:22:54 +0530

    GET Catch params in NG

--- src/app/pages/show-details/show-details.component.ts
+++ src/app/pages/show-details/show-details.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-show-details',
   templateUrl: './show-details.component.html',
-  styleUrl: './show-details.component.scss'
+  styleUrl: './show-details.component.scss',
 })
-export class ShowDetailsComponent {
+export class ShowDetailsComponent implements OnInit {
+  showId = 0;
 
+  constructor(private router: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.router.params.subscribe((params) => {
+      this.showId = params['id'];
+    });
+  }
 }
